8be47f930f56e2b6420113a77479772250d966e9
hello <hewebgl3@qq.com>
2020-03-11 16:12:40 +0800

    加入mychart
    mychart

--- helm_install.sh
+++ helm_install.sh
@@ -3,7 +3,7 @@
 helm_install2(){
 
     HELM_VERSION=v2.14.3
-    wget xxxx
+    wget https://raw.githubusercontent.com/hewebgl3/helm-hellodemos.com/master/helm-v2.14.3-linux-amd64.tar.gz
     tar -zxvf helm-v2.14.3-linux-amd64.tar.gz
     mv linux-amd64/helm /usr/local/bin/helm
 
